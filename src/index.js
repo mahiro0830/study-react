b2cd61ec8da08e006ea496c554a01e274ba5adad
峯岸真弘 <shishisus4@gmail.com>
2024-04-03 18:19:58 +0900

    231

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-  root.render(
-    <p>現在時刻：{new Date().toLocaleString()}</p>
-  );
-}, 1000);
+root.render(
+  <>
+    <p>こんにちは、世界！</p>
+    <p>こんにちは、React！</p>
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
