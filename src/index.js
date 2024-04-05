1de61419303f92bfac76b228bdfb9283de228f79
峯岸真弘 <shishisus4@gmail.com>
2024-04-05 20:07:22 +0900

    ディレクトリ整理

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MyHello from './MyHello';
+import MyHello from './03/MyHello';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
