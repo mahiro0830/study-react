2e47c45e16cdf7a22502bdccec6a67fb7538222b
峯岸真弘 <shishisus4@gmail.com>
2024-04-11 00:48:44 +0900

    propTypesで型宣言をして、エラーがでることを確認

--- src/03/MyHello.js
+++ src/03/MyHello.js
@@ -1,5 +1,13 @@
-export default function MyHello( { myName } ) {
+import PropTypes from 'prop-types';
+
+const MyHello = ( { myName } ) => {
   return (
     <div>こんにちは、{ myName }さん！</div>
   );
 }
+
+MyHello.propTypes = {
+  myName: PropTypes.string.isRequired,
+}
+
+export default MyHello;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import ListTemplate from './03/ListTemplate';
-import books from './03/books';
+import MyHello from './03/MyHello';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ListTemplate src={ books } render={
-    el => (
-      <React.Fragment>
-        <dt>
-          <a href={ `https://google.co.jp/${ el.isbn }` }>
-            { el.title }（{ el.price }円）
-          </a>
-        </dt>
-        <dd>{ el.summary }</dd>
-      </React.Fragment>
-    )
-  }>
-  </ListTemplate>
+  <MyHello>
+  </MyHello>
 );
