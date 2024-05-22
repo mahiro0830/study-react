c483365ea9c1e758a9d985a1d87abc63b22f09ff
峯岸真弘 <shishisus4@gmail.com>
2024-05-22 14:25:34 +0900

    「Control + Q」が押下されたときにヘルプメッセージを表示させる

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import EventPoint from './03/EventPoint';
+import EventKey from './03/EventKey';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <EventPoint />
+  <EventKey />
 );
