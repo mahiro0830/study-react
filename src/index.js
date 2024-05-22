55d79f7f7be8b1d551ad8f5335d0f415617e7c72
峯岸真弘 <shishisus4@gmail.com>
2024-05-22 12:31:16 +0900

    Stateを用いて、マウスの出入りで画像が入れ替わるようにする

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import StateParent from './03/StateParent';
+import EventMouse from './03/EventMouse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StateParent>
-  </StateParent>
+  <EventMouse beforeSrc="https://www.web-deli.com/image/linkbanner_l.gif"
+    afterSrc="https://www.web-deli.com/image/home_chara.gif" alt="ロゴ画像" />
 );
