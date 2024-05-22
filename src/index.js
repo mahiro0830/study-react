816c721595c90dc910960019e8d1d7bd9f8f81c5
峯岸真弘 <shishisus4@gmail.com>
2024-05-22 14:17:15 +0900

    イベント発生時のマウスの位置をリアルタイムで表示する（Stateの利用）

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import EventMouse from './03/EventMouse';
+import EventPoint from './03/EventPoint';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <EventMouse beforeSrc="https://www.web-deli.com/image/linkbanner_l.gif"
-    afterSrc="https://www.web-deli.com/image/home_chara.gif" alt="ロゴ画像" />
+  <EventPoint />
 );
