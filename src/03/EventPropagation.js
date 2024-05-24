9a0b1022738decbf67a7f56b95bab31d2f0359d7
峯岸真弘 <shishisus4@gmail.com>
2024-05-24 16:34:54 +0900

    イベントの伝播を抑制する
    e.stopPropagation();

--- src/03/EventPropagation.js
+++ src/03/EventPropagation.js
@@ -3,14 +3,18 @@ import './css/EventPropagation.css';
 const EventPropagation = () => {
   const handleParent = () => alert('#parent run...');
   const handleMy = () => alert('#my run...');
-  const handleChild = () => alert('#child run...');
+  const handleChild = e => {
+    e.stopPropagation();
+    e.preventDefault();
+    alert('#child run...');
+  };
 
   return (
-    <div id='parent' onClickCapture={handleParent}>
+    <div id='parent' onClick={handleParent}>
       親要素
-      <div id='my' onClickCapture={handleMy}>
+      <div id='my' onClick={handleMy}>
         現在要素
-        <a id='child' href='https://google.com' onClickCapture={handleChild}>
+        <a id='child' href='https://google.com' onClick={handleChild}>
           子要素
         </a>
       </div>
