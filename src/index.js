1e08ac0d326bfef344d8ce9e45621b22ccc03be2
峯岸真弘 <shishisus4@gmail.com>
2024-05-26 14:25:08 +0900

    dir変更

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import FormTextarea from './03/FormTextarea';
+import FormTextarea from './04/FormTextarea';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
