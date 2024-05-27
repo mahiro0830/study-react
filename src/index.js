eb424124b41c23ef825023a07cbcf47ea4a6c880
峯岸真弘 <shishisus4@gmail.com>
2024-05-28 01:21:28 +0900

    Immerライブラリを利用して、Stateの更新を簡略化する

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import StateNest from './04/StateNest';
+import StateNestImmer from './04/StateNestImmer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StateNest />
+  <StateNestImmer />
 );
