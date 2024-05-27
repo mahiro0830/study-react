6cccdda2a0bcb395ce4d4aac7d1688b65c43cd6c
峯岸真弘 <shishisus4@gmail.com>
2024-05-27 12:32:29 +0900

    リストボックスを制御コンポネントで実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import FormSelect from './04/FormSelect';
+import FormList from './04/FormList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <FormSelect />
+  <FormList />
 );
