6d4c4fa58f1ed352f372dea286b113eaa58187cd
峯岸真弘 <shishisus4@gmail.com>
2024-06-23 12:20:10 +0900

    useErrorBoundaryを利用して、例外なエラーをErrorBoundaryに渡す。（try~catch構文）

--- src/05/ErrorEvent.js
+++ src/05/ErrorEvent.js
@@ -1,7 +1,14 @@
+import { useErrorBoundary } from "react-error-boundary";
+
 const ErrorEvent = () => {
   // エラーを発生させるやばいボタン
+  const { showBoundary } = useErrorBoundary();
   const handleClick = () => {
-    throw new Error('Error is occured in MyApp.');
+    try {
+      throw new Error('Error is occured in MyApp.');
+    } catch (e) {
+      showBoundary(e);
+    }
   };
   return (
     <button type="button" onClick={ handleClick }>エラーを発生させる</button>
