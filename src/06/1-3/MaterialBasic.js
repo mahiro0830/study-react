f0c89b9b9f8f0d5ba258036369915c23ab184ff4
峯岸真弘 <shishisus4@gmail.com>
2024-06-24 13:38:23 +0900

    MUIのButtonコンポネントを使用してみる

---
+++ src/06/1-3/MaterialBasic.js
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+const MaterialBasic = () => {
+  return (
+    <>
+      <Button variant="text" color="secondary">Text</Button>
+      <Button variant="contained" color="secondary">Contained</Button>
+      <Button variant="outlined" color="secondary">Outlined</Button>
+    </>
+  );
+};
+
+export default MaterialBasic;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import ErrorEventRoot from './05/ErrorEventRoot';
+// import ErrorEventRoot from './05/ErrorEventRoot';
+
+import MaterialBasic from './06/1-3/MaterialBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
     <div id="dialog"></div>
-    <ErrorEventRoot />
+    <MaterialBasic />
   </>
 );
