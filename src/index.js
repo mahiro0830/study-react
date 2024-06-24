ee06921ec27be98831005f45d7445e92de820f78
峯岸真弘 <shishisus4@gmail.com>
2024-06-24 13:55:13 +0900

    ディレクトリ修正

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 // import ErrorEventRoot from './05/ErrorEventRoot';
 
-import MaterialBasic from './06/1-3/MaterialBasic';
+import MaterialBasic from './06/1-2/MaterialBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
